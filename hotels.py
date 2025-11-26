c4efa63df6b44dbaf4e730c1a511530a18d6d0aa
Vlad <vlad.ivanov.path@gmail.com>
2025-11-26 15:18:20 +0300

    task: pagination

--- hotels.py
+++ hotels.py
@@ -5,16 +5,29 @@ router = APIRouter(prefix='/hotels', tags=["Отели"])
 
 
 hotels_data_base = [{'id': 1, 'title': 'Sochi', 'name': "Сочи Плаза"},
-          {'id': 2, 'title': 'Dubai','name':'Дубай Гранд Отель'}
+          {'id': 2, 'title': 'Dubai','name':'Дубай Гранд Отель'},
+          {'id': 3, 'title': 'Sochi', 'name': "Сочи Плаза"},
+          {'id': 4, 'title': 'Sochi', 'name': "Сочи Плаза"},
+          {'id': 5, 'title': 'Dubai','name':'Дубай Гранд Отель'},
+          {'id': 6, 'title': 'Sochi', 'name': "Сочи Плаза"},
+          {'id': 7, 'title': 'Sochi', 'name': "Сочи Плаза"},
+          {'id': 8, 'title': 'Dubai','name':'Дубай Гранд Отель'},
+          {'id': 9, 'title': 'Sochi', 'name': "Сочи Плаза"},
+          {'id': 10, 'title': 'Sochi', 'name': "Сочи Плаза"},
+          {'id': 11, 'title': 'Dubai','name':'Дубай Гранд Отель'},
+          {'id': 12, 'title': 'Sochi', 'name': "Сочи Плаза"},
           ]
 
 
 @router.get("/")
 def func(id:int| None = Query(default=None, description="Id отеля"),
-    title:str| None = Query(default=None, description="Название отеля")
+    title:str| None = Query(default=None, description="Название отеля"),
+    page_size:int | None = Query(default=6, description="Размер пагинации для страницы"),
+    page:int | None = Query(default=1, description="Нумерация страницы")
          ):
 
     hotels_ = []
+    start_pagination, end_pagination = page_size*(page-1), page_size*page
 
     for hotel in hotels_data_base:
         if id and hotel['id'] != id:
@@ -24,7 +37,7 @@ def func(id:int| None = Query(default=None, description="Id отеля"),
 
         hotels_.append(hotel)
 
-    return(hotels_)
+    return(hotels_[start_pagination:end_pagination])
 
 @router.post("/")
 def add_hotel(hotel_data:Hotels = Body(openapi_examples={
